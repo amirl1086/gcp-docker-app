c8c6e10c7fef673a2128347c5317f4e3e256755c
alavi86 <alavi@synamedia.com>
2021-12-25 16:21:37 +0200

    hardcoded docker username in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-docker build -t $DOCKER_USERNAME/complex-client:latest -t $DOCKER_USERNAME/complex-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t $DOCKER_USERNAME/complex-server:latest -t $DOCKER_USERNAME/complex-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t $DOCKER_USERNAME/complex-worker:latest -t $DOCKER_USERNAME/complex-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+# TODO - env variables
 
-docker push $DOCKER_USERNAME/complex-client:latest
-docker push $DOCKER_USERNAME/complex-client:$GIT_SHA
-docker push $DOCKER_USERNAME/complex-server:latest
-docker push $DOCKER_USERNAME/complex-server:$GIT_SHA
-docker push $DOCKER_USERNAME/complex-worker:latest
-docker push $DOCKER_USERNAME/complex-worker:$GIT_SHA
+docker build -t amirl1086/complex-client:latest -t amirl1086/complex-client:$GIT_SHA -f ./client/Dockerfile ./client
+docker build -t amirl1086/complex-server:latest -t amirl1086/complex-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t amirl1086/complex-worker:latest -t amirl1086/complex-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push amirl1086/complex-client:latest
+docker push amirl1086/complex-client:$GIT_SHA
+docker push amirl1086/complex-server:latest
+docker push amirl1086/complex-server:$GIT_SHA
+docker push amirl1086/complex-worker:latest
+docker push amirl1086/complex-worker:$GIT_SHA
 
 # possible to replace the images in the config files before
 kubectl apply -f ./orchestration
-kubectl set image deploymet/client-deploymet client=$DOCKER_USERNAME/complex-client:$GIT_SHA
-kubectl set image deploymet/server-deploymet server=$DOCKER_USERNAME/complex-server:$GIT_SHA
-kubectl set image deploymet/worker-deploymet worker=$DOCKER_USERNAME/complex-worker:$GIT_SHA
+kubectl set image deploymet/client-deploymet client=amirl1086/complex-client:$GIT_SHA
+kubectl set image deploymet/server-deploymet server=amirl1086/complex-server:$GIT_SHA
+kubectl set image deploymet/worker-deploymet worker=amirl1086/complex-worker:$GIT_SHA
